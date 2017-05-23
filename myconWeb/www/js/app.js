98d20ac2763c249ebde1c29625086fee10746864
Francisco Alvarado Ferllini <ferllini_13@hotmail.com>
2017-05-22 18:15:08 -0600

    CDN's necesarios y manejo ejemplo de las views

--- myconWeb/www/js/app.js
+++ myconWeb/www/js/app.js
@@ -1,24 +1,96 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
+var web_sevice_ip = ""; 
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.config( function ( $stateProvider , $urlRouterProvider ){
+    
+	$stateProvider.state('login', {
+                url:'/login',
+                controller: 'controlador_login',
+                templateUrl:'templates/login.html'
+            })
+    $stateProvider.state('inicio', {
+                url:'/inicio',
+                controller: 'controlador_inicio',
+                templateUrl:'html/inicio.html'
+            })
+    
+      $urlRouterProvider.otherwise('/inicio');
 })
+   
+
+
+.controller('controladro_login', function($scope, $state, loginData,$http){
+    $scope.login = {username:'', password:'',name:'',id:'', rol='' };
+        var form = document.getElementById("myForm");  
+        form.onsubmit = function(){
+        form.reset();
+      }
+		
+		
+		
+    $scope.verificar =  function(login){
+		
+		//lo comentado son las llamadas que vamos a combiar del WS
+					/*
+                                $http.get(request)
+                        .then(function (response) {
+                        var data = response.data;
+                        var result = data.substring(76, data.length - 9);
+    					//si el mae si exite, se procede...
+                    
+                            var result2 = angular.fromJson(result);
+                            updateRoles(login,result2[0]._id,result2[0]._name,result2[0]._office,loginData)
+
+                        }
+
+                    });  */ 
+    };
+    
+    function updateRoles(login,id,name,office,loginData){
+      /*  
+		var request="";
+    	$http.post(request)
+            .then(function (response) {
+            var data = response.data;
+            var result = data.substring(76, data.length - 9);
+            var result4 = angular.fromJson(result);
+		
+            rol=result4.rol_id);
+                                    }
+			//acomodar roles
+            if (rol==0){  
+            loginData.updateLogin(login,id,name,office,rol);
+                
+            $state.go('inicio');
+            }
+         }); */
+
+};
+})
+
+
+
+//hay que modificar lo de abajo en nombres segun fotmato y lo que se va a guardar XD
+
+
+.service('login_datos', function() {
+return {
+login: {},
+getLogin: function() {// para recuperar los datos 
+ 	return this.login;
+},
+	
+updateLogin: function(login,id,name,rol) {// para actualizar los datos del login, y definir el tipo de menu segun tipo de usuario
+   	this.login = login;// username, password
+   	this.login.id=id;
+   	this.login.name=name;
+	this.login.name=name;
+}
+}
+})
+
+.directive('menu', function() {
+  return {
+     templateUrl: 'templates/menu.html',
+      controller:"controlador_menu"
+  };});
