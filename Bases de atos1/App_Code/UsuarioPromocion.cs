d6b710f7f8bad7d5e69314f7a457cbd759db3b66
JasonLeiton <ljjasonj017@gmail.com>
2017-05-28 17:17:10 -0600

    conexion con twitter y clases basicas

---
+++ Bases de atos1/App_Code/UsuarioPromocion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de UsuarioPromocion
+/// </summary>
+[Serializable]
+public class UsuarioPromocion
+{
+    public UsuarioPromocion()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
