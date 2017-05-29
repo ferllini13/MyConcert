eaeb27fa1566734756d853923488c70859966941
JasonLeiton <ljjasonj017@gmail.com>
2017-05-28 19:16:35 -0600

    creacion de clases, falta implementar las restricciones

---
+++ Bases de atos1/App_Code/Banda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Banda
+/// </summary>
+[Serializable]
+public class Banda
+{
+    private string nombre { get; set; }
+    private List<string> miembros { get; set; }
+    private List<string> cancionesPrincipales { get; set; }
+    private string foto { get; set; }
+
+
+    public Banda()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
