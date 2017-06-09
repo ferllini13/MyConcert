811b17bd887de25adc06a7888459234da0a683a8
jairoM26 <jairomendezmartinez@gmail.com>
2017-06-09 14:13:58 -0600

    se añade algo

---
+++ MyConcert/ManejoImagenes/SubirImagen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace MyConcert.ManejoImagenes
+{
+    FileUpload fu;
+
+    
+    public class SubirImagen 
+    {
+
+        
+    }
+}
